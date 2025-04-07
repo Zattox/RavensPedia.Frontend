e92127e7c8574c710e50707f1ef58b5bebbc40cf
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-04-07 23:17:54 +0300

    Use npx prettier --write .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,19 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-    "./index.html"
-  ],
-    theme: {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./index.html"],
+  theme: {
     extend: {
       colors: {
         blue: {
-          500: '#3b82f6',
-          600: '#2563eb',
+          500: "#3b82f6",
+          600: "#2563eb",
         },
         green: {
-          500: '#10b981',
-          600: '#059669',
+          500: "#10b981",
+          600: "#059669",
         },
         // Добавьте другие цвета по необходимости
       },
     },
   },
   plugins: [],
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import fs from 'fs';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     port: 5173,
     https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem'),
+      key: fs.readFileSync("./certs/key.pem"),
+      cert: fs.readFileSync("./certs/cert.pem"),
     },
   },
 });
